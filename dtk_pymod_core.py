e2d3c74a4a01f9b9501427060a64682703abe93c
Sv3tlana <stitova@intven.com>
2020-03-16 11:51:23 -0700

    cleaned up and made code pep8 compliant and more "pythonic".

--- dtk_pymod_core.py
+++ dtk_pymod_core.py
@@ -16,43 +16,52 @@ susceptible = []
 recovered = []
 disdeaths = []
 
+
 def setup_vd_callbacks():
-    gi.set_mortality_callback( mortality_callback )
-    nd.set_conceive_baby_callback( conceive_baby_callback )
-    nd.set_update_preg_callback( update_pregnancy_callback )
+    gi.set_mortality_callback(mortality_callback)
+    nd.set_conceive_baby_callback(conceive_baby_callback)
+    nd.set_update_preg_callback(update_pregnancy_callback)
 
-def conceive_baby_callback( individual_id, duration ):
-    print( "{0} just got pregnant".format( individual_id ) )
-    gi.initiate_pregnancy( individual_id )
 
-def update_pregnancy_callback( individual_id, dt ):
-    return gi.update_pregnancy( individual_id, int(dt) )
+def conceive_baby_callback(individual_id):
+    print("{0} just got pregnant".format(individual_id))
+    gi.initiate_pregnancy(individual_id)
 
-def mortality_callback( age, sex ):
-    #print( "Getting mortality rate for age {0} and sex {1}.".format( age, sex ) )
-    mortality_rate = nd.get_mortality_rate( ( age, sex ) )
+
+def update_pregnancy_callback(individual_id, dt):
+    return gi.update_pregnancy(individual_id, int(dt))
+
+
+def mortality_callback(age, sex):
+    # print( "Getting mortality rate for age {0} and sex {1}.".format( age, sex ) )
+    mortality_rate = nd.get_mortality_rate((age, sex))
     return mortality_rate
 
-def is_incubating( person ):
-    if gi.is_infected( person ) and gi.get_infectiousness( person ) == 0:
+
+def is_incubating(person):
+    if gi.is_infected(person) and gi.get_infectiousness(person) == 0:
         return True
     else:
         return False
 
+
 fertility = None
-def do_shedding_update( human_pop ):
+
+
+def do_shedding_update(human_pop):
     # This is not optimal. Only optimize if it really helps
     global fertility
     if fertility is None:
-        config = json.loads( open( "nd.json" ).read() )
+        config = json.loads(open("nd.json").read())
         fertility = config["Enable_Birth"] and config["Enable_Vital_Dynamics"]
     stat_pop = 0
     for human in human_pop:
         hum_id = human["id"]
-        mcw = gi.get_mcw( hum_id )
-        #Can we make this conditional on 
+        mcw = gi.get_mcw(hum_id)
+        # Can we make this conditional on
         if fertility:
-            nd.update_node_stats( ( mcw, 0, gi.is_possible_mother(hum_id), 0 ) ) # mcw, infectiousness, is_poss_mom, is_infected
+            nd.update_node_stats(
+                (mcw, 0, gi.is_possible_mother(hum_id), 0))  # mcw, infectiousness, is_poss_mom, is_infected
         """
         if gi.is_infected(hum_id):
             if gi.has_latent_infection(hum_id):
@@ -63,25 +72,31 @@ def do_shedding_update( human_pop ):
             print( "{0} has no infection.".format( hum_id ) )
         """
         stat_pop += mcw
-        if gi.is_infected( hum_id ):
-            gi.update1( hum_id ) # this should do shedding & vital-dynamics
+        if gi.is_infected(hum_id):
+            gi.update1(hum_id)  # this should do shedding & vital-dynamics
     return stat_pop
 
-def save_output( tag ): #, susceptible, exposeds, prevalence, recovered, disdeaths ):
+
+def save_output(tag):  # , susceptible, exposeds, prevalence, recovered, disdeaths ):
     filename = os.path.join(os.getcwd(), "output", tag + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.csv'))
-    if not os.path.exists( "output" ):
-        os.mkdir( "output" )
-    datadict = { "S": susceptible, "E": exposeds, "I": prevalence, "R": recovered, "D": disdeaths } 
-    
+    if not os.path.exists("output"):
+        os.mkdir("output")
+    datadict = {"S": susceptible, "E": exposeds, "I": prevalence, "R": recovered, "D": disdeaths}
+
     # Design choice: Not imposing pandas dependency
-    with open( filename, 'w' ) as datafile: 
-        mywriter = csv.DictWriter( datafile,fieldnames=["S","E","I","R","D"] )
+    with open(filename, 'w') as datafile:
+        mywriter = csv.DictWriter(datafile, fieldnames=["S", "E", "I", "R", "D"])
         mywriter.writeheader()
         for idx in range(len(datadict["S"])):
-            mywriter.writerow( {'S': datadict['S'][idx], 'E': datadict["E"][idx], 'I': datadict["I"][idx], 'R': datadict["R"][idx], 'D': datadict["D"][idx]})
+            mywriter.writerow(
+                {'S': datadict['S'][idx],
+                 'E': datadict["E"][idx],
+                 'I': datadict["I"][idx],
+                 'R': datadict["R"][idx],
+                 'D': datadict["D"][idx]})
 
 
-def do_vitaldynamics_update( human_pop, graveyard, contagion, census_cb = None, death_cb = None ):
+def do_vitaldynamics_update(human_pop, graveyard, contagion, census_cb=None, death_cb=None):
     num_infected = 0
     num_incubating = 0
     num_active = 0
@@ -93,62 +108,63 @@ def do_vitaldynamics_update( human_pop, graveyard, contagion, census_cb = None, 
 
     for human in human_pop:
         hum_id = human["id"]
-        if census_cb != None:
-            census_cb( hum_id )
+        if census_cb:
+            census_cb(hum_id)
 
-        gi.update2( hum_id ) # this should do exposure; possible optimization would be to skip this entirely if zero contagion
+        gi.update2(hum_id)  # this should do exposure; possible optimization would be to
+        # skip this entirely if zero contagion
 
-        mcw = gi.get_mcw( hum_id )
-        if gi.is_dead( hum_id ):
+        mcw = gi.get_mcw(hum_id)
+        if gi.is_dead(hum_id):
             # somebody died
-            print( "{0} is dead.".format( hum_id ) )
-            new_graveyard.append( human )
-            num_deaths += mcw # can't use len(graveyard) coz mcw
-            if death_cb != None:
-                death_cb( hum_id )
+            print("{0} is dead.".format(hum_id))
+            new_graveyard.append(human)
+            num_deaths += mcw  # can't use len(graveyard) coz mcw
+            if death_cb:
+                death_cb(hum_id)
 
         num_people += mcw
         global fertility
         if fertility:
-            ipm = gi.is_possible_mother( hum_id )
-            ip = gi.is_pregnant( hum_id )
+            ipm = gi.is_possible_mother(hum_id)
+            ip = gi.is_pregnant(hum_id)
             if hum_id == 0:
                 pdb.set_trace()
-            age = gi.get_age( hum_id )
-            #print( "Calling cfp with {0}, {1}, {2}, and {3}.".format( str(ipm), str(ip), str(age), str(hum_id) ) )
+            age = gi.get_age(hum_id)
+            # print( "Calling cfp with {0}, {1}, {2}, and {3}.".format( str(ipm), str(ip), str(age), str(hum_id) ) )
             # TBD: Optimization? I happen to know that this is only necessary for females of a 
             # certain age. But technically that knowledge is for nd.
-            nd.consider_for_pregnancy( ( ipm, ip, hum_id, age, 1.0 ) )
+            nd.consider_for_pregnancy((ipm, ip, hum_id, age, 1.0))
 
-        #print( str( json.loads(gi.serialize( hum_id ))["individual"]["susceptibility"] ) )
-        if gi.is_infected( hum_id ):
-            if is_incubating( hum_id ):
+        # print( str( json.loads(gi.serialize( hum_id ))["individual"]["susceptibility"] ) )
+        if gi.is_infected(hum_id):
+            if is_incubating(hum_id):
                 num_incubating += mcw
             else:
-                num_infected += mcw # TBD: use_mcw
-        elif gi.get_immunity( hum_id ) != 1.0:
-            num_recover += mcw # TBD: use mcw
+                num_infected += mcw  # TBD: use_mcw
+        elif gi.get_immunity(hum_id) != 1.0:
+            num_recover += mcw  # TBD: use mcw
         else:
-            num_suscept += mcw # TBD: use mcw
-            #if gi.has_active_infection( hum_id ):
+            num_suscept += mcw  # TBD: use mcw
+            # if gi.has_active_infection( hum_id ):
             #    num_active += 1
         # serialize seems to be broken when you had an intervention (or at least a SimpleVaccine)
-        #serial_man = gi.serialize( hum_id )
-        #if hum_id == 1:
-            #print( json.dumps( json.loads( serial_man ), indent=4 ) )
-            #print( "infectiousness: " + str( json.loads( serial_man )["individual"]["infectiousness"] ) )
-    #print( "Updating fertility for this timestep." )
+        # serial_man = gi.serialize( hum_id )
+        # if hum_id == 1:
+        # print( json.dumps( json.loads( serial_man ), indent=4 ) )
+        # print( "infectiousness: " + str( json.loads( serial_man )["individual"]["infectiousness"] ) )
+    # print( "Updating fertility for this timestep." )
     for corpse in new_graveyard:
         if corpse in human_pop:
-            human_pop.pop( human_pop.index( corpse ) )
-        else: 
-            print( "Exception trying to remove individual from python list: " + str( ex ) )
-    graveyard.extend( new_graveyard )
+            human_pop.pop(human_pop.index(corpse))
+        else:
+            print("Exception trying to remove individual from python list: " + str(ex))
+    graveyard.extend(new_graveyard)
     nd.update_fertility()
-    exposeds.append( num_incubating )
-    prevalence.append( num_infected )
-    active_prevalence.append( num_active )
-    susceptible.append( num_suscept )
-    recovered.append( num_recover )
-    disdeaths.append( num_deaths )
-    #gc.collect()
+    exposeds.append(num_incubating)
+    prevalence.append(num_infected)
+    active_prevalence.append(num_active)
+    susceptible.append(num_suscept)
+    recovered.append(num_recover)
+    disdeaths.append(num_deaths)
+    # gc.collect()
--- transmission_demo.py
+++ transmission_demo.py
@@ -23,13 +23,13 @@ to be introduced.
 
 # Initialize module variables.
 human_pop = []
-nursery = {} # store newborn counter by timestep (dict of int to int for now)
+nursery = {}  # store newborn counter by timestep (dict of int to int for now)
 timestep = 0
-random.seed( 445 ) # I like the number 4
+random.seed(445)  # I like the number 4
 
 # constants/parameters
 vaccine_disribution_timestep = 2
-outbreak_timesteps = [ 1 ]
+outbreak_timesteps = [1]
 outbreak_coverage = 0.002
 sim_duration = 180
 CHILD = 0
@@ -37,11 +37,11 @@ ADULT = 1
 close_schools_timestep = 54
 
 contagion_buckets = [0, 0]
-#contagion_bucket_homog = 0
+# contagion_bucket_homog = 0
 
-#FROM_CHILD_TO_CHILD = 0.25
-#FROM_CHILD_TO_ADULT = 0.75
-#FROM_ADULT_TO_CHILD = 0.75
+# FROM_CHILD_TO_CHILD = 0.25
+# FROM_CHILD_TO_ADULT = 0.75
+# FROM_ADULT_TO_CHILD = 0.75
 FROM_CHILD_TO_CHILD = 1.00
 FROM_CHILD_TO_ADULT = 1.00
 FROM_ADULT_TO_CHILD = 1.00
@@ -50,9 +50,10 @@ FROM_ADULT_TO_ADULT = 1.00
 factors = [[FROM_CHILD_TO_CHILD, FROM_ADULT_TO_CHILD],
            [FROM_CHILD_TO_ADULT, FROM_ADULT_TO_ADULT]]
 
-ADULT_CHILD_AGE_THRESHOLD=7300
+ADULT_CHILD_AGE_THRESHOLD = 7300
 
-def create_person_callback( mcw, age, gender ):
+
+def create_person_callback(mcw, age, gender):
     """
     This callback is required by dtk_nodedemographic during population initialization.
     It can be named anything as long as it is registered via the appropriate callback.
@@ -64,33 +65,31 @@ def create_person_callback( mcw, age, gender ):
         age = random.randrange(ADULT_CHILD_AGE_THRESHOLD, 36500)
     # print("Creating {}.".format("ADULT" if age >= ADULT_CHILD_AGE_THRESHOLD else "CHILD"))
     # TODO: move some of this to core.
-    global human_pop # make sure Python knows to use module-level variable human_pop
-    #global timestep
+    global human_pop  # make sure Python knows to use module-level variable human_pop
+    # global timestep
     global nursery
-    person = {}
-    person["mcw"]=mcw
-    person["age"]=age/365
-    person["sex"]=gender
+    person = {"mcw": mcw, "age": age / 365, "sex": gender}
 
-    new_id = gi.create( gender, age, mcw ) 
-    person["id"]=new_id
-    human_pop.append( person )
+    new_id = gi.create(gender, age, mcw)
+    person["id"] = new_id
+    human_pop.append(person)
     if age == 0:
-        month_step = int( timestep/30.0 )
-        #print( "Made a baby on timestep {0}/month {1}.".format( str( timestep ), str( month_step ) ) )
+        month_step = int(timestep / 30.0)
+        # print( "Made a baby on timestep {0}/month {1}.".format( str( timestep ), str( month_step ) ) )
         if month_step not in nursery:
-            nursery[month_step] = ( 0, 0 )
+            nursery[month_step] = (0, 0)
         boys = nursery[month_step][0]
         girls = nursery[month_step][1]
         if gender == 0:
             boys += mcw
         else:
-            girls += mcw 
-        nursery[month_step] = ( boys, girls )
+            girls += mcw
+        nursery[month_step] = (boys, girls)
+
 
 # INTERESTING TRANSMISSION CODE STARTS HERE
 
-def expose_callback( individual_id ):
+def expose_callback(individual_id):
     """
     This function is the callback for exposure. It is registered with the intrahost module
     and then called for each individual for each timestep. This is where you decide whether 
@@ -102,32 +101,34 @@ def expose_callback( individual_id ):
     """
 
     # The following code is just to demo the use of TBHIV-specific getters
-    if gi.is_infected( individual_id ): 
-        #print( "Individual {0} is apparently already infected.".format( individual_id ) )
+    if gi.is_infected(individual_id):
+        # print( "Individual {0} is apparently already infected.".format( individual_id ) )
         return 0
 
-    if gi.get_immunity( individual_id ) == 0:
+    if gi.get_immunity(individual_id) == 0:
         return 0
 
     global timestep
-    #print( "timestep = {0}, outbreak_timestep = {1}.".format( timestep, outbreak_timestep ) )
-    #if timestep in outbreak_timesteps:
-    #    #if gi.get_immunity( individual_id ) == 1.0 and random.random() < 0.1: # let's infect some people at random (outbreaks)
+    # print( "timestep = {0}, outbreak_timestep = {1}.".format( timestep, outbreak_timestep ) )
+    # if timestep in outbreak_timesteps:
+    #    if gi.get_immunity( individual_id ) == 1.0 and random.random() < 0.1:
+    #    let's infect some people at random (outbreaks)
+
     #    if random.random() < outbreak_coverage: # let's infect some people at random (outbreaks)
     #        print( "Let's force-infect (outbreak) uninfected, non-immune individual based on random draw." )
     #        return 1
     if (timestep == 1) and (individual_id == 13):
-        return 1    # force-infect individual 13 at time 0
+        return 1  # force-infect individual 13 at time 0
     else:
         if individual_id == 0:
             pdb.set_trace()
 
-        global contagion_buckets 
-        #global contagion_bucket_homog
+        global contagion_buckets
+        # global contagion_bucket_homog
 
-        #print( "Exposing individual {0} to contagion {1}.".format( individual_id, contagion_bucket ) )
+        # print( "Exposing individual {0} to contagion {1}.".format( individual_id, contagion_bucket ) )
 
-        #HINT-y code here
+        # HINT-y code here
         contagion = contagion_buckets[CHILD] + contagion_buckets[ADULT]
 
         me = ADULT if gi.get_age(individual_id) >= ADULT_CHILD_AGE_THRESHOLD else CHILD
@@ -135,18 +136,20 @@ def expose_callback( individual_id ):
         my_factor_from_child = factors[me][CHILD]
         my_factor_from_adult = factors[me][ADULT]
 
-        contagion = (contagion_buckets[CHILD] * my_factor_from_child) + (contagion_buckets[ADULT] * my_factor_from_adult)
+        contagion = (contagion_buckets[CHILD] * my_factor_from_child) + (
+                    contagion_buckets[ADULT] * my_factor_from_adult)
         contagion /= len(human_pop)
-        #print( "HINT-y math: I am an {}, exposed to contagion {}.".format( "CHILD" if me == CHILD else "ADULT", contagion ) )
-        #contagion = contagion_bucket_homog
-        
+        # print( "HINT-y math: I am an {}, exposed to contagion {}.
+        # ".format( "CHILD" if me == CHILD else "ADULT", contagion ) )
+        # contagion = contagion_bucket_homog
 
-        if gi.should_infect( ( individual_id, contagion ) ):
+        if gi.should_infect((individual_id, contagion)):
             return 1
 
     return 0
 
-def deposit_callback( contagion, individual_id ):
+
+def deposit_callback(contagion, individual_id):
     """
     This function is the callback for shedding. It is registered with the intrahost module
     and then called for each shedding individual for each timestep. This is where you collect
@@ -154,32 +157,35 @@ def deposit_callback( contagion, individual_id ):
     value is only populated with non-zero values if pymod-specific SetGeneticID code is 
     turned on in the DTK. This is only example I can think of of pymod-specific code in DTK.
     """
-    #print( "{0} depositing {1} contagion creates total of {2}.".format( individual, contagion, well_mixed_contagion_pool ) )
-    #print( "{0} depositing {1} contagion.".format( individual, contagion ) )
+    # print( "{0} depositing {1} contagion creates total of {2}.".format( individual,
+    #                                                                  contagion, well_mixed_contagion_pool ) )
+    # print( "{0} depositing {1} contagion.".format( individual, contagion ) )
     # Figure out what age bucket the individual is in.
-    if individual_id == 0: # we need the DTK code to be built with the GeneticID used for the individual id. If not, this will be 0
-        pdb.set_trace() 
-    
-    #print( "Shedding {0} into age-clan index {1}.".format( contagion, index ) )
-    #age_of_infection = gi.get_infection_age( individual_id )
-    #contagion = get_infectiousness( age_of_infection )
+    if individual_id == 0:  # we need DTK code to be built with the GeneticID used for the ind id. If not, this will be 0
+        pdb.set_trace()
+
+        # print( "Shedding {0} into age-clan index {1}.".format( contagion, index ) )
+    # age_of_infection = gi.get_infection_age( individual_id )
+    # contagion = get_infectiousness( age_of_infection )
     global contagion_buckets
-    #global contagion_bucket_homog
+    # global contagion_bucket_homog
     bucket_index = ADULT if gi.get_age(individual_id) >= ADULT_CHILD_AGE_THRESHOLD else CHILD
     contagion_buckets[bucket_index] += contagion
-    #contagion_bucket_homog += contagion
+    # contagion_bucket_homog += contagion
 
-def publish_callback( human_id, event_id ):
+
+def publish_callback(human_id, event_id):
     """
     This function gets invoked by DTK module events but the plubming is currently disconnected
     pending perf improvements.
     """
     pass
-    #print( "Broadcast event {} on {}.".format( event_id, human_id ) )
+    # print( "Broadcast event {} on {}.".format( event_id, human_id ) )
 
-# INTERESTING TRANSMISSION CODE ENDS HERE 
 
-def get_infectiousness( age_of_infection ):
+# INTERESTING TRANSMISSION CODE ENDS HERE
+
+def get_infectiousness(age_of_infection):
     """
     The get_infectiousness function is an optional demo function that lets you create customized infectiousness profiles.
     The input parameter is the age of infection (for an individual) and it returns a floating point value.
@@ -189,49 +195,51 @@ def get_infectiousness( age_of_infection ):
     # you can do whatever functional form you want here. 
     inf = 0
     if age_of_infection < 30:
-        inf = 0.1 * (age_of_infection)/30.
-    elif age_of_infection >= 30 and age_of_infection < 60:
-        inf = 0.1 * (60-age_of_infection)/30.
-    #print( "inf = {0}.".format( inf ) )
+        inf = 0.1 * age_of_infection / 30.
+    elif 30 <= age_of_infection < 60:
+        inf = 0.1 * (60 - age_of_infection) / 30.
+    # print( "inf = {0}.".format( inf ) )
     return inf
 
-# 
+
+#
 # INTERVENTION HELPERS
 # 
 
-def distribute_interventions( t ):
+def distribute_interventions(t):
     """
     Function to isolated distribution of interventions to individuals.
     Interventions are separate python modules. 
     """
     if t == close_schools_timestep:
-        print( "SCHOOL CLOSURE INTERVENTION" )
+        print("SCHOOL CLOSURE INTERVENTION")
         FROM_CHILD_TO_CHILD = 0.25
         FROM_CHILD_TO_ADULT = 0.75
         FROM_ADULT_TO_CHILD = 0.75
-        #FROM_ADULT_TO_ADULT = 0
+        # FROM_ADULT_TO_ADULT = 0
         global factors
         factors = [[FROM_CHILD_TO_CHILD, FROM_ADULT_TO_CHILD],
                    [FROM_CHILD_TO_ADULT, FROM_ADULT_TO_ADULT]]
 
     if t == vaccine_disribution_timestep:
         for human in human_pop:
-            hum_id = human["id"] 
+            hum_id = human["id"]
 
             # Below is code to give out anti-tb drugs
-            #individual_ptr = gi.get_individual( hum_id )
-            #print( "Giving anti-tb drug to {0}.".format( hum_id ) ) 
-            #tdi.distribute( individual_ptr )
+            # individual_ptr = gi.get_individual( hum_id )
+            # print( "Giving anti-tb drug to {0}.".format( hum_id ) )
+            # tdi.distribute( individual_ptr )
 
             # Below is code to giveout ART via function that resets ART timers
-            #give_art( hum_id )
+            # give_art( hum_id )
 
-            #Below is code to give out vaccines; this should be updated to use the distribute method
-            #print( "Giving simple vaccine to {0}.".format( hum_id ) )
-            #vaccine = vi.get_intervention()
-            #gi.give_intervention( ( hum_id, vaccine ) )
-            if gi.get_age( hum_id ) < 70*365:
-                vi.distribute( gi.get_individual_for_iv( hum_id ) )
+            # Below is code to give out vaccines; this should be updated to use the distribute method
+            # print( "Giving simple vaccine to {0}.".format( hum_id ) )
+            # vaccine = vi.get_intervention()
+            # gi.give_intervention( ( hum_id, vaccine ) )
+            if gi.get_age(hum_id) < 70 * 365:
+                vi.distribute(gi.get_individual_for_iv(hum_id))
+
 
 def setup_callbacks():
     """
@@ -239,13 +247,14 @@ def setup_callbacks():
     to invoke for vital dynamics and transmission.
     """
     # set creation callback
-    nd.set_callback( create_person_callback )
+    nd.set_callback(create_person_callback)
     # set vital dynamics callbacks
-    gi.my_set_callback( expose_callback )
-    gi.set_deposit_callback( deposit_callback )
+    gi.my_set_callback(expose_callback)
+    gi.set_deposit_callback(deposit_callback)
     setup_vd_callbacks()
 
-def run( from_script = False ):
+
+def run(from_script=False):
     """
     This is the main function that actually runs the demo, as one might expect. It does the folllowing:
     - Register callbacks
@@ -258,54 +267,54 @@ def run( from_script = False ):
         - "Migration"
     - Plot simple reports summarizing results
     """
-    global human_pop # make sure Python knows to use module-level variable human_pop
+    global human_pop  # make sure Python knows to use module-level variable human_pop
     del human_pop[:]
 
     setup_callbacks()
-    nd.populate_from_files() 
+    nd.populate_from_files()
     if len(human_pop) == 0:
-        print( "Failed to create any people in populate_from_files. This isn't going to work!" )
+        print("Failed to create any people in populate_from_files. This isn't going to work!")
         sys.exit(0)
 
     graveyard = []
     global timestep
     global contagion_buckets
     dis_death_cum = 0
-    #global contagion_bucket_homog 
-    for t in range(0,sim_duration): # for one year
+    # global contagion_bucket_homog
+    for t in range(0, sim_duration):  # for one year
         timestep = t
 
-        stat_pop = do_shedding_update( human_pop )
-        #contagion_bucket_homog /= len(human_pop)
-        do_vitaldynamics_update( human_pop, graveyard, contagion_buckets ) 
-        distribute_interventions( t )
+        stat_pop = do_shedding_update(human_pop)
+        # contagion_bucket_homog /= len(human_pop)
+        do_vitaldynamics_update(human_pop, graveyard, contagion_buckets)
+        distribute_interventions(t)
 
         dis_death_cum += disdeaths[-1]
-        print( f"At end of timestep {t} num_infected = {prevalence[-1]} stat_pop = {stat_pop}, disease deaths = {dis_death_cum}." )
+        print(f"At end of timestep {t} num_infected = {prevalence[-1]} stat_pop = {stat_pop}, "
+              f"disease deaths = {dis_death_cum}.")
         contagion_buckets = [0, 0]
-        #contagion_bucket_homog = 0
+        # contagion_bucket_homog = 0
 
     # Sim done: Report.
     # save prevalence with tag and timestamp
-    tag = (sys.argv[1] + "_") if len( sys.argv ) > 1 else ""  # secret tag option
-    save_output( tag )
+    tag = (sys.argv[1] + "_") if len(sys.argv) > 1 else ""  # secret tag option
+    save_output(tag)
 
     # This could get moved to core but users might want to play with it.
-    plt.plot( susceptible, color='green', label='S' )
-    plt.plot( exposeds, color='purple', label='E' )
-    plt.plot( prevalence, color='orange', label='I' )
-    plt.plot( recovered, color='blue', label='R' )
-    plt.plot( disdeaths, color='red', label='D' )
-    plt.xlabel( "time" )
+    plt.plot(susceptible, color='green', label='S')
+    plt.plot(exposeds, color='purple', label='E')
+    plt.plot(prevalence, color='orange', label='I')
+    plt.plot(recovered, color='blue', label='R')
+    plt.plot(disdeaths, color='red', label='D')
+    plt.xlabel("time")
     plt.legend()
     plt.show()
 
     if from_script:
-        print( "NURSERY\n" + json.dumps( nursery, indent=4 ) )
-        print( "GRAVEYARD\n" + json.dumps( graveyard, indent=4  ) )
+        print("NURSERY\n" + json.dumps(nursery, indent=4))
+        print("GRAVEYARD\n" + json.dumps(graveyard, indent=4))
     return graveyard
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     run()
-    
